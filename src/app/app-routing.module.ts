8428ed9d7086b94f43d5bdb4e742b42d8273ea7d
Ahmed Mohamed Elhelw <48494438+ahmed-elhelw@users.noreply.github.com>
2022-12-07 20:09:09 +0200

    add auth guard function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { AuthGuard } from './user/auth.guard';
 import { LoginComponent } from './user/login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: 'products',
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
     component: ProductListComponent
   },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
