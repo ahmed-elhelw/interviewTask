6294693bd4a0b99350ba22c665b626cf32527a22
Ahmed Mohamed Elhelw <48494438+ahmed-elhelw@users.noreply.github.com>
2022-12-07 19:53:22 +0200

    product functionality, add filter by category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPaginationModule } from 'ngx-pagination';
 
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { LoginComponent } from './user/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
     ProductListComponent,
+    PageNotFoundComponent,
     LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgxPaginationModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, catchError, combineLatest, EMPTY, map } from 'rxjs';
+import { ProductCategoryService } from '../product-category/product-category.service';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  //p is used for pagination
+  p: number = 1;
+  errorMessage = '';
+  checkedIdx = -1;
+
+
+  private SelectedCategorySubject = new BehaviorSubject<string>('');
+  SelectedCategoryAction$ = this.SelectedCategorySubject.asObservable();
+
+
+
+
+  products$ = this.productService.products$
+
+  categories$ = this.productCategoryService.categories$
+  .pipe(
+    catchError(err =>{
+       this.errorMessage = err;
+       return EMPTY;
+      })
+  );
+
+
+  filteredProducts$ = combineLatest([
+    this.products$,
+    this.SelectedCategoryAction$,
+  ]).pipe(
+      map(([products, selectedCatId]) =>
+        products.filter((product:any) =>
+          selectedCatId? product.category === selectedCatId : true,
+        )),
+      catchError(err =>{
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
+
+
+
+  constructor(private productService: ProductService,
+    private productCategoryService: ProductCategoryService) {
+  }
+
+  onChangeCategory(event:any): void {
+    let categoryName = event.target.value
+    this.SelectedCategorySubject.next(categoryName);
+    console.log(event.target.value);
+    console.log(event.target.checked);
+  }
 
 }
