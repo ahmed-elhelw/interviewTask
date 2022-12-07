1349ed41258ea7294861e87a968f0f5ec331a8cc
Ahmed Mohamed Elhelw <48494438+ahmed-elhelw@users.noreply.github.com>
2022-12-07 20:04:52 +0200

    add login

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  currentUser?: User | undefined;
+  redirectUrl = '';
+
+  get isLoggedIn(): boolean {
+    return !!this.currentUser;
+  }
 
   constructor() { }
+
+  login(userName: string, password: string): void {
+    if (!userName || !password) {
+      return;
+    }
+    if (userName === 'admin') {
+      this.currentUser = {
+        id: 1,
+        userName,
+        isAdmin: true
+      };
+      return;
+    }
+    this.currentUser = {
+      id: 2,
+      userName,
+      isAdmin: false
+    };
+  }
+
+  logout(): void {
+    this.currentUser = undefined;
+  }
 }
