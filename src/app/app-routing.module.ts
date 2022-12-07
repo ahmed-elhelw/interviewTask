be25aeec14b9df0406c1c3b0b6a7d5ac27ec7f6c
Ahmed Mohamed Elhelw <48494438+ahmed-elhelw@users.noreply.github.com>
2022-12-07 19:31:56 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { LoginComponent } from './user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'products',
+    // canActivate: [AuthGuard],
+    component: ProductListComponent
+  },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
     ProductListComponent,
     LoginComponent
   ],
